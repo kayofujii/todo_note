078442951dbee0900ece9335b5fceedab31ae06d
kayofujii <kayosamu1025@gmail.com>
2022-01-08 17:35:14 +0900

    タスク追加機能を追加

--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
+from .forms import TaskForm
 from .models import Task
 
 
@@ -8,4 +9,11 @@ class TaskView(View):
     def get(self, request):
         params = {}
         params["task"] = Task.objects.all()
+        params["form"] = TaskForm()
         return render(request, "task_list.html", params)
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("task_list")
